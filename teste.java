708fb4b4b996a20334e8da81c40f975e76488355
Felipe Rosa <feelipe-r@hotmail.com>
2020-09-18 02:16:21 -0300

    Create teste.java

---
+++ teste.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class teste{
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Informe um nome:");
+    String nome = sc.nextLine();
+    
+    System.out.println("Hello "+nome+"!");
+   }
+ }
