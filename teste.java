48bf2e36c1a2c801366888d88b327e6626be2880
Felipe Rosa <feelipe-r@hotmail.com>
2020-09-18 02:46:47 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
 public class teste{
-  public static void main(String[]args){
+    public static void main(String[]args){
     Scanner sc = new Scanner(System.in);
     
     System.out.println("Informe um nome:");
     String nome = sc.nextLine();
     
     System.out.println("Hello "+nome+"!");
-   }
- }
+   
+    }
+}
